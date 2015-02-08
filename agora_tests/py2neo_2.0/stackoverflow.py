f1b3b6ad7a09b49da90ce9b1c18cc3b50a2717b4
Marnee <marnee.dearman@gmail.com>
2015-02-08 16:12:49 -0700

    refactored some file names and added tokens to the user json

--- agora_tests/py2neo_2.0/stackoverflow.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Marnee Dearman'
-from py2neo import Graph
-user_node = Graph().find_one('USER',
-                             property_key='email',
-                             property_value='marnee@agorasociety.com')
-# user_properties['mission_statement'] = 'New mission statement'
-# user_node.bind(uri=Graph().uri)
-user_node.properties['mission_statement'] = 'New mission statement'
-user_node.push()
