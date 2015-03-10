c7b6aba3b12e3025f7453a1e18c40440330903ad
marnee <marnee.dearman@gmail.com>
2015-03-10 16:18:48 -0700

    Progress #3 Add search by name for group, interest, user

--- agora_api/app.py
+++ agora_api/app.py
@@ -29,22 +29,22 @@ def cors_middleware(request, response, params):
 
 api = application = falcon.API(after=[crossdomain], before=[cors_middleware])
 
-user_profile = api_users.UserProfile()
-user_interests = api_users.UserInterests()
 user = api_users.User()
+user_locations = api_users.UserLocations()
+
+user_interests = api_users.UserInterests()
 user_goals = api_users.UserGoals()
 user_interest_goals = api_users.UserInterestGoals()
 local_shared_users = api_users.LocalUsersSharedInterests()
-user_locations = api_users.UserLocations()
 user_groups = api_users.UserGroups()
 user_organizations = api_users.UserOrganizations()
-interest = api_interests.Interest()
+interests = api_interests.Interest()
 location = api_locations.Location()
 location_interests = api_locations.LocationInterests()
 location_users = api_locations.LocationUsers()
 location_groups = api_locations.LocationGroups()
 location_organizations = api_locations.LocationOrganizations()
-group = api_groups.Group()
+groups = api_groups.Group()
 group_users = api_groups.GroupUsers()
 group_interests = api_groups.GroupInterests()
 group_goals = api_groups.GroupGoals()
@@ -58,46 +58,66 @@ activate = api_users.ActivateUser()
 
 # api.add_route('/login/{token}')
 
-#USER SPECIFIC
-# api.add_route('/users/profile/{email}', user_profile)
-#get all the stuff related to my user
-# api.add_route('/users/activate/{payload}', activate)
-# api.add_route('/users/register/{email}')
+# USER COLLECTIONS
+
+# this will create a user account if it does not exist
 api.add_route('/activate/', activate)
-api.add_route('/users/', user)  # use this route to register/login users
-api.add_route('/users/{email}', user)
-api.add_route('/users/{email}/locations', user_locations)
+
+# use this route to register/login users
+#GET users based on query string field=''
+api.add_route('/users/', user)
+# GET and POST user data
+api.add_route('/users/{user_id}', user)
+
+# USER LOCATIONS
+api.add_route('/users/{user_id}/locations', user_locations)
+api.add_route('/users/{user_id}/locations/{location_id}', user_locations)
+
+# USER INTERESTS
 api.add_route('/users/{user_id}/interests', user_interests)
+# GET a list of interests for the user
 api.add_route('/users/{user_id}/interests/{interest_id}', user_interests)
-api.add_route('/users/{email}/interests/{interest_id}/goals', user_interest_goals)
-api.add_route('/users/{email}/interests/{interest_id}/goals/{goal_id}', user_interest_goals)
-api.add_route('/users/{email}/interests/{interest_id}/goals', user_interest_goals)
-# api.add_route('/users/{email}/interests', user_interests)
+# GET a view of the
+api.add_route('/users/{user_id}/interests/{interest_id}/goals', user_interest_goals)
+api.add_route('/users/{user_id}/interests/{interest_id}/goals/{goal_id}', user_interest_goals)
+api.add_route('/users/{user_id}/interests/{interest_id}/goals', user_interest_goals)
 
-api.add_route('/users/{email}/goals/{goal_id}', user_goals)
-api.add_route('/users/{email}/goals', user_goals)
+# USER GOALS
+api.add_route('/users/{user_id}/goals/{goal_id}', user_goals)
+api.add_route('/users/{user_id}/goals', user_goals)
 
-#get general location view -- share interests, shared interest groups, shared organizations
-#get stuff related to the location  (paged?)
+# USER GROUPS
+api.add_route('/users/{user_id}/groups/{group_id}', user_groups)
+api.add_route('/users/{user_id}/groups', user_groups)
+
+#INTERESTS
+api.add_route('/interests', interests)
+api.add_route('/interests/{interest_id}', interests)
+
+# get general location view -- share interests, shared interest groups, shared organizations
+# get stuff related to the location  (paged?)
+
+# LOCATION COLLECTIONS
 api.add_route('/locations/{place_id}/interests', location_interests)
 api.add_route('/locations/{place_id}/interests/{interest_id}/users', location_users)
 api.add_route('/locations/{place_id}/interests/{interest_id}/groups', location_groups)
 api.add_route('/locations/{place_id}/interests/{interest_id}/organizations', location_organizations)
 
-api.add_route('/users/{user_id}/groups/{group_id}', user_groups)
-api.add_route('/users/{user_id}/groups', user_groups)
-
-api.add_route('/groups/{group_id}', group)
+# GROUPS COLLECTIONS
+api.add_route('/groups/', groups)
+api.add_route('/groups/{group_id}', groups)
 api.add_route('/groups/{group_id}/goals', group_goals)
 api.add_route('/groups/{group_id}/achievements', group_achievements)
 api.add_route('/groups/{group_id}/users', group_users)
 api.add_route('/groups/{group_id}/interests', group_interests)
 
+# ORGANIZATION COLLECTIONS
+
 api.add_route('/organizations/{org_id}', organization)
 api.add_route('/organizations/{org_id}/interests', org_interests)
 api.add_route('/organizations/{org_id}/users', org_users)
 
-#get users with shared interests with shared location
+# get users with shared interests with shared location
 # api.add_route('/locations/{place_id}/interests/{interest_id}/users', )
 
 # api.add_route('/interests/{interest_id}', interest)
