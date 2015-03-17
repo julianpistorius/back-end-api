3b883c94d8dc9aa8c10333014ae29bcafd8afc46
marnee <marnee.dearman@gmail.com>
2015-03-17 14:24:43 -0700

    Progress #19 Add JSON validation to POST and PUT -- added meeting validation

--- agora_api/api_meetings.py
+++ agora_api/api_meetings.py
@@ -6,6 +6,7 @@ from agora_db.auth import Auth
 from agora_db.meeting import AgoraMeeting
 from agora_db.group import AgoraGroup
 from api_serializers import MeetingResponder
+from validators import validate_meeting_schema
 import simplejson
 
 
@@ -48,16 +49,19 @@ class Meeting(object):
             if group.allow_edit(auth_key=auth.auth_key):
                 raw_json = request.stream.read()
                 result_json = simplejson.loads(raw_json, encoding='utf-8')
-                #TODO check against json schema -- when is best to do this?
-                meeting = AgoraMeeting()
-                meeting.set_meeting_properties(result_json['meeting'])
-                meeting.create_meeting(group_id=group_id)
+                if validate_meeting_schema.validate_meeting(result_json):
+                    meeting = AgoraMeeting()
+                    meeting.set_meeting_properties(result_json['meeting'])
+                    meeting.create_meeting(group_id=group_id)
+                    response.content_type = 'application/json'
+                    response.status = falcon.HTTP_201
+                else:
+                    response.status = falcon.HTTP_400
             else:
                 response.status = falcon.HTTP_401
         else:
             response.status = falcon.HTTP_401
-        response.content_type = 'application/json'
-        response.status = falcon.HTTP_201
+
 
     def on_put(self, request, response, group_id, meeting_id):
         auth = user_auth(request)
@@ -70,12 +74,16 @@ class Meeting(object):
             if group.allow_edit(auth_key=auth.auth_key):
                 raw_json = request.stream.read()
                 result_json = simplejson.loads(raw_json, encoding='utf-8')
-                meeting.set_meeting_properties(result_json['meeting'])
-                meeting.update_meeting()
+                if validate_meeting_schema.validate_meeting(result_json):
+                    meeting.set_meeting_properties(result_json['meeting'])
+                    meeting.update_meeting()
+                    response.content_type = 'application/json'
+                    response.status = falcon.HTTP_201
+                else:
+                    response.status = falcon.HTTP_400
             else:
                 response.status = falcon.HTTP_401
         else:
             response.status = falcon.HTTP_401
-        response.content_type = 'application/json'
-        response.status = falcon.HTTP_201
+
 
--- validators/validate_conversation_response_schema.py
+++ validators/validate_conversation_response_schema.py
@@ -1,1 +1,43 @@
 __author__ = 'marnee'
+from jsonschema import validate
+
+conversation_schema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "id": "/",
+  "type": "object",
+  "properties": {
+    "conversation": {
+      "id": "conversation",
+      "type": "object",
+      "properties": {
+        "subject": {
+          "id": "subject",
+          "type": "string"
+        },
+        "message": {
+          "id": "message",
+          "type": "string"
+        }
+      },
+      "required": [
+        "subject",
+        "message"
+      ]
+    }
+  },
+  "required": [
+    "conversation"
+  ]
+}
+
+
+#TODO return object with why json validation failed
+
+
+def validate_conversation(conversation_json):
+    try:
+        validate(conversation_json, conversation_schema)
+        return True
+    except:
+        return False
+
