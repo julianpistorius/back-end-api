58918793a1ea3b53e1f3ba12c8ec0c78c418a4b6
marnee <marnee.dearman@gmail.com>
2015-03-18 14:55:32 -0700

    #4 Refactor directory and class names to reflect new organization name -- fixed a BDD unrelated to this issue

--- features/steps/activate_user.py
+++ features/steps/activate_user.py
@@ -11,19 +11,23 @@ import settings
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 
+
 @given(u'an activation URL')
 def an_activation_url(context):
     context.activation_url = context.base_url + settings.ACTIVATION_ROUTE
 
+
 @given(u'email for persona "new_user"')
 def step_impl(context):
-    context.activation_email = context.personas['new_user']['email']
+    context.activation_email = context.personas['user']['email']
+
 
 # @given(u'I have an activation URL with valid token')
 @given(u'URL payload is valid')
 def step_impl(context):
     s = URLSafeTimedSerializer(secret_key=settings.TOKEN_SECRET_KEY)
     context.payload = s.dumps(context.activation_email)
+
 
 @when(u'Client POST to "/users/activation" with body')
 def post_activation_url(context):
@@ -49,38 +53,35 @@ def step_impl(context):
     activated_user_response_json = context.response.json()
     schema = {
         "$schema": "http://json-schema.org/draft-04/schema#",
-        "id": "",
+        "id": "http://elmerly.com/",
         "type": "object",
         "properties": {
-            "user": {
-                "id": "/user",
+            "activationkey": {
+                "id": "http://elmerly.com//activationkey",
                 "type": "object",
                 "properties": {
-                    "permanent_web_token": {
-                        "id": "/user/permanent_web_token",
-                        "type": "string"
-                    },
-                    "id": {
-                        "id": "/user/id",
+                    "x_auth_key": {
+                        "id": "http://elmerly.com//activationkey/x_auth_key",
                         "type": "string"
                     }
                 },
                 "required": [
-                    "permanent_web_token",
-                    "id"
+                    "x_auth_key"
                 ]
             }
         },
         "required": [
-            "user"
+            "activationkey"
         ]
     }
     validate(activated_user_response_json, schema)
+
 
 @given(u'URL payload is invalid')
 def invalid_token(context):
     s = URLSafeTimedSerializer(secret_key=settings.TOKEN_SECRET_KEY)
     context.payload = s.dumps('bad-email')
+
 
 @then(u'The response should be 400 Bad request')
 def step_impl(context):
